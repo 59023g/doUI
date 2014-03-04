9ac36b39aea019a3697d3f49c545f51580ae0b56
mp1erce <hell@michaelepierce.com>
2014-03-04 12:31:13 -0800

    They're talking!
    moved express configs to lib/config for modularity. fixed rootPath.
    disabled caching for dev. installed connect-mongo for sessions

--- public/js/controllers/doController.js
+++ public/js/controllers/doController.js
@@ -1,8 +1,8 @@
  'use strict';
 
  angular.module('doUI')
-   .controller('doController', function($scope, game) {
-     $scope.games = game.query();
+   .controller('doController', function($scope) {
+     //$scope.games = game.query();
 
      $scope.timerRunning = true;
      var timeStarted = false;
@@ -38,51 +38,3 @@
      };
    });
 
-
- /*var doUI = angular.module('doUI', ['timer']);
-
- var doController = angular.module('doController', []);
-
-
-
- doController.controller('doController', ['$scope', 'game',
-     function($scope, game) {
-         $scope.games = game.query();
-
-         $scope.timerRunning = true;
-         var timeStarted = false;
-         $scope.countdownVal = 900;
-         var count = 0;
-
-         $scope.startClock = function() {
-             if (!timeStarted) {
-                 $scope.$broadcast('timer-start');
-                 $scope.timerRunning = true;
-                 timeStarted = true
-             } else if ((timeStarted) && (!$scope.timerRunning)) {
-                 $scope.$broadcast('timer-resume');
-                 $scope.timerRunning = true;
-             }
-
-         };
-
-         $scope.stopClock = function() {
-             if ((timeStarted) && ($scope.timerRunning)) {
-                 $scope.$broadcast('timer-stop');
-                 $scope.timerRunning = false;
-             }
-
-         };
-
-         $scope.resetClock = function() {
-             if ((!$scope.timerRunning))
-                 $scope.$broadcast('timer-reset');
-
-         }
-
-         $scope.addPoint = function() {
-            $scope.games.put();
-         }
-     }
- ]);
-*/
